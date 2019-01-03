72a9ff5b8e01cdd7ea754c641504ee8b3762763a
Bluemi <bruno.schilling@protonmail.ch>
2019-01-03 22:56:16 +0100

    added MessageServiceTest and PersonServiceTest

---
+++ messenger/messenger-test/src/test/java/de/sb/messenger/rest/MessageServiceTest.java
@@ -0,0 +1,15 @@
+package de.sb.messenger.rest;
+
+public class MessageServiceTest extends ServiceTest {
+    public void testFilterQueries() {
+        // TODO
+    }
+
+    public void testRelations() {
+        // TODO
+    }
+
+    public void testLifeCycle() {
+        // TODO
+    }
+}
---
+++ messenger/messenger-test/src/test/java/de/sb/messenger/rest/PersonServiceTest.java
@@ -0,0 +1,15 @@
+package de.sb.messenger.rest;
+
+public class PersonServiceTest extends ServiceTest {
+    public void testFilterQueries() {
+        // TODO
+    }
+
+    public void testRelations() {
+        // TODO
+    }
+
+    public void testLifeCycle() {
+        // TODO
+    }
+}
