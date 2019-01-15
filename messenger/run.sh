5d9b3fe16cf2787fd9bf4e93cf9d2a5904509c80
Bluemi <bruno.schilling@protonmail.ch>
2019-01-15 11:57:41 +0100

    ...

--- messenger/run.sh
+++ messenger/run.sh
@@ -18,31 +18,3 @@ xdotool search --name "Mozilla Firefox" windowactivate
 xdotool key alt+1
 sleep 2.8
 xdotool key ctrl+r
-sleep 1
-
-xdotool mousemove 164 236
-xdotool click 1
-
-setxkbmap de
-xdotool type sascha.baumeister@gmail.com
-
-xdotool mousemove 564 236
-xdotool click 1
-
-xdotool mousemove 164 268
-xdotool click 1
-
-xdotool type sascha
-
-xdotool mousemove 83 307
-xdotool click 1
-
-sleep 1.1
-
-xdotool mousemove 324 153
-xdotool click 1
-
-# click ines
-# sleep 0.3
-# xdotool mousemove 90 270
-# xdotool click 1
